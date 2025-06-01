23623c5f18610b49d5e69e2e15cf9f4b8c0d5523
rushichavan21 <rushichavan2327@gmail.com>
2025-06-01 18:45:57 +0530

    defined url model

---
+++ server/src/models/url.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const urlSchema = new mongoose.Schema({
+  originalUrl: {
+    type: String,
+    required: true,
+  },
+  shortUrl: {
+    type: String,
+    required: true,
+    index:true,
+    unique: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  clicks: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+});
+
+const url_Schema = mongoose.model("Url_Schema", urlSchema);
+export default url_Schema;
