8ebcadccf4f49f40a661add3f1d7d9dd146ba550
rushichavan21 <rushichavan2327@gmail.com>
2025-06-04 18:53:04 +0530

    built the centeral part

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/navbar'
+import CenterPart from './components/CenterPart'
+
+const App = () => {
+  return (
+    <div className='mix-h-[100vh] '>
+      <Navbar/>
+      <CenterPart/>
+    </div>
+  )
+}
+
+export default App
