0585ca63089eef145bca75f33ad8ed875f448458
rushichavan21 <rushichavan2327@gmail.com>
2025-06-08 16:39:32 +0530

    new routes

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ dotenv.config("./.env");
 import connectToMongo from "./src/config/mongo.config.js";
 import url_Schema from "./src/models/url.model.js";
 import shortUrlRouter from "./src/routes/shortUrl.route.js";
+import authRouter from "./src/routes/auth.route.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -13,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/generate-id",shortUrlRouter);
 app.use("/",shortUrlRouter);
-
+app.use("/auth",authRouter)
 
 app.listen(3000, () => {
   connectToMongo();
