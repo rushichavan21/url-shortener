c3ecebcd61e425f56cd4c0661c0326e8f10a857d
rushichavan21 <rushichavan2327@gmail.com>
2025-06-10 17:17:15 +0530

    creafted pages

---
+++ client/src/pages/landingpage/Landingpage.jsx
@@ -0,0 +1,13 @@
+
+import Navbar from '../../components/navbar/Navbar'
+import CenterPart from '../../components/centerPart/CenterPart'
+import "./landingpage.css";
+const Landingpage = () => {
+  return (
+    <div className='landing-wrapper'><Navbar/>
+    <CenterPart/>
+    </div>
+  )
+}
+
+export default Landingpage
