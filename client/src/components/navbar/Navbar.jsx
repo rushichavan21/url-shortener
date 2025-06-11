e383d3b75c9a9b84225a5255e25f83ed9d639628
rushichavan21 <rushichavan2327@gmail.com>
2025-06-11 13:02:19 +0530

    conditional navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,25 +1,53 @@
+import { useNavigate, useRouterState } from '@tanstack/react-router';
 import './navbar.css';
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from '../../context/AuthContext';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  
+
+
+  const pathname = useRouterState({ select: state => state.location.pathname });
+
+  const { dispatch , user} = useContext(AuthContext);
+  console.log(user);
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    dispatch({ type: 'LOGOUT' });
+    navigate({ to: '/' });
+  };
+
+  const isDashboard = pathname.startsWith('/dashboard');
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <div className="navbar-left">
           <div className="navbar-brand">
-            
-            <span className="logo-url"><img src="./logo-url.svg" alt="" /></span>
+            <span className="logo-url"><img src="./logo-url.svg" alt="Logo" /></span>
             <span className="brand-text">Url Shortner</span>
           </div>
           <div className="navbar-links">
-            <a href="#">contact us</a>
+            <a href="#">Contact Us</a>
             <a href="#">Learn</a>
-            <a href="#">Learn how this works?</a> 
+            <a href="#">How this works?</a>
           </div>
         </div>
+
         <div className="navbar-right">
-          <button className="btn login-btn">Log in</button>
-          <button className="btn signup-btn">Sign up</button>
+          {!isDashboard ? (
+            <>
+              <button className="btn login-btn" onClick={() => navigate({ to: '/auth/login' })}>Log in</button>
+              <button className="btn signup-btn" onClick={() => navigate({ to: '/auth/signup' })}>Sign up</button>
+            </>
+          ) : (
+            <span>
+              <span className='btn navbar-email'>{user?.email}</span>
+              <button className="btn login-btn" onClick={handleLogout}>Logout</button>
+            </span>
+          )}
         </div>
       </div>
     </nav>
