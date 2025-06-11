9ff29bbdf37e439c6763a5b463daa92e7b0ec928
rushichavan21 <rushichavan2327@gmail.com>
2025-06-11 13:01:21 +0530

    dashboard page modified

--- client/src/pages/dashboard/DashboardPage.jsx
+++ client/src/pages/dashboard/DashboardPage.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import Navbar from '../../components/navbar/Navbar'
+import './dashboard.css'
+import CenterPart from '../../components/centerPart/CenterPart'
+import Options from '../../components/options/Options'
 const DashboardPage = () => {
   return (
-    <div>DashboardPage</div>
+    <div className='dashboard-page-wrapper'>
+      <Navbar/>
+      <Options/>
+    </div>
   )
 }
 
