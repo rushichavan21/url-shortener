dba1cfcaf500b518950c347f7e101a213d29aac3
rushichavan21 <rushichavan2327@gmail.com>
2025-06-12 14:38:26 +0530

    calling and mapping

--- client/src/components/viewAll/ViewAll.jsx
+++ client/src/components/viewAll/ViewAll.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
-
+import React from 'react';
+import { useAuthContext } from '../../hooks/useAuthContextHook';
+import { fetchAllUrls } from '../../api/shortUrl.api';
+import { useQuery } from '@tanstack/react-query';
+import UrlCard from '../urlCard/UrlCard';
+import "./viewall.css"
 const ViewAll = () => {
-  return (
-    <div>ViewAll</div>
-  )
-}
+  const { user } = useAuthContext();
 
-export default ViewAll
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ['urls'],
+    queryFn: () => fetchAllUrls(user.token),
+    enabled: !!user?.token,
+  });
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error: {error.message}</div>;
+
+  return (
+    <div>
+      <div className="viewall-container">
+     {data?.urls.map((url) => (
+    <UrlCard key={url.shortUrl} data={url} />
+  ))}
+      </div>
+    
+    </div>
+  );
+};
+
+export default ViewAll;
