e8788cf1233ec4804561167ed1030b5d535bb198
rushichavan21 <rushichavan2327@gmail.com>
2025-06-12 10:57:56 +0530

    new controller for  user with token

--- server/src/controller/shortUrl.controller.js
+++ server/src/controller/shortUrl.controller.js
@@ -1,4 +1,4 @@
-import {shortUrlserviceWithOutUser}from '../services/shortUrl.service.js';
+import {shortUrlserviceCustom, shortUrlserviceWithOutUser, shortUrlserviceWithUser}from '../services/shortUrl.service.js';
 
 export const shortUrlController = async (req, res) => {
   const { url } = req.body;
@@ -14,3 +14,44 @@ export const shortUrlController = async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 }
+
+export const shortUrlWithUserController = async (req, res) => {
+  const { url } = req.body;
+  const userId = req.user?.userId;
+
+  if (!url) {
+    return res.status(400).json({ error: 'URL is required' });
+  }
+
+  if (!userId) {
+    return res.status(401).json({ error: 'Unauthorized: User not found' });
+  }
+
+  try {
+    const shortUrl = await shortUrlserviceWithUser(url, userId);
+    res.status(200).json({ shortUrl: process.env.APP_URL + shortUrl });
+  } catch (error) {
+    console.error('Error saving URL for user:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+export const shortUrlCustomController = async (req, res) => {
+   const { url ,customId } = req.body;
+  const userId = req.user?.userId;
+
+  if (!url) {
+    return res.status(400).json({ error: 'URL is required' });
+  }
+
+  if (!userId) {
+    return res.status(401).json({ error: 'Unauthorized: User not found' });
+  }
+  try {
+    const shortUrl = await shortUrlserviceCustom(url, userId,customId);
+    res.status(200).json({ shortUrl: process.env.APP_URL + shortUrl });
+  } catch (error) {
+    console.error('Error saving URL for user:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
