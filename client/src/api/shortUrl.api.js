8d45f127b57591d0dd948c535aa903f2428d39ee
rushichavan21 <rushichavan2327@gmail.com>
2025-06-12 10:56:54 +0530

    more calls

--- client/src/api/shortUrl.api.js
+++ client/src/api/shortUrl.api.js
@@ -3,5 +3,33 @@ export const getShortUrl = async (url) => {
   const response=await axiosInstance.post("/api/generate-id", {
     url: url,
   });
-    return response.data;
+    return response.data.shortUrl;
+}
+
+export const getShortUrlWithUser = async (url, token) => {
+  const response = await axiosInstance.post(
+    "/create/user",
+    { url },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  console.log("Response from getShortUrlWithUser:", response.data.shortUrl);
+  return response.data.shortUrl
+};
+
+export const getShortUrlCustom = async (url, customId, token) => {
+  const response = await axiosInstance.post(
+    "/create/user/custom",
+    { url, customId },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  console.log("Response from getShortUrlCustom:", response.data);
+  return response.data.shortUrl;
 }
