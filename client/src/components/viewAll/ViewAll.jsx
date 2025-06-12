927dfe0bbc2bede881aa8fc4681feb4ca2ccb5b9
rushichavan21 <rushichavan2327@gmail.com>
2025-06-12 15:03:53 +0530

    view All modified

--- client/src/components/viewAll/ViewAll.jsx
+++ client/src/components/viewAll/ViewAll.jsx
@@ -3,7 +3,8 @@ import { useAuthContext } from '../../hooks/useAuthContextHook';
 import { fetchAllUrls } from '../../api/shortUrl.api';
 import { useQuery } from '@tanstack/react-query';
 import UrlCard from '../urlCard/UrlCard';
-import "./viewall.css"
+import "./viewall.css";
+
 const ViewAll = () => {
   const { user } = useAuthContext();
 
@@ -13,17 +14,28 @@ const ViewAll = () => {
     enabled: !!user?.token,
   });
 
-  if (isLoading) return <div>Loading...</div>;
-  if (isError) return <div>Error: {error.message}</div>;
+  if (isLoading) {
+    return <div className="loader">Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  const urls = data?.urls || [];
 
   return (
-    <div>
-      <div className="viewall-container">
-     {data?.urls.map((url) => (
-    <UrlCard key={url.shortUrl} data={url} />
-  ))}
-      </div>
-    
+    <div className='viewall-wrapper'>
+      <h2>All Short URLs</h2>
+      {urls.length === 0 ? (
+        <p>You haven’t created any URLs yet.</p>
+      ) : (
+        <div className="viewall-container">
+          {urls.map((url) => (
+            <UrlCard key={url.shortUrl} data={url} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
