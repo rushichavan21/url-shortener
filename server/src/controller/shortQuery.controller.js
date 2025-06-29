57fcf8c134467a8dcca5214326052abdcbf785c5
rushichavan21 <rushichavan2327@gmail.com>
2025-06-29 23:04:45 +0530

    cache req

--- server/src/controller/shortQuery.controller.js
+++ server/src/controller/shortQuery.controller.js
@@ -1,16 +1,29 @@
 import url_Schema from "../models/url.model.js";
-import { updateClicks } from "../dao/shortQuery.dao.js";
+import { getUrlfromDb,updateClicks } from "../dao/shortQuery.dao.js";
 import { normalizeOriginalUrl } from "../utils/solver.js";
+import { redisClient } from "../cache/redisClient.js";
+import url_Schema from "../models/url.model.js";
+import { getUrlfromDb, updateClicks } from "../dao/shortQuery.dao.js";
+import { normalizeOriginalUrl } from "../utils/solver.js";
+import { redisClient } from "../cache/redisClient.js";
 
 export const shortQueryController = async (req, res) => {
   try {
-      const shortUrl = req.params.id;
-      console.log("Short URL received:", shortUrl);
-    let original= await updateClicks(shortUrl);
-     original= normalizeOriginalUrl(original);
-      res.redirect(original);
-    } catch (error) {
-      console.error("Error during redirection:", error);
-      res.status(500).send("Server error");
+    const shortUrl = req.params.id;
+    console.log("Short URL received:", shortUrl);
+    let original = await redisClient.get(shortUrl);
+    if (!original) {
+      console.log("Cache miss for short URL:", shortUrl);
+      original = await getUrlfromDb(shortUrl);
+      if (!original) return res.status(404).send("Short URL not found");
+      await redisClient.set(shortUrl, original, { EX: 3600 });
     }
-}
+    updateClicks(shortUrl).catch(console.error);
+    original = normalizeOriginalUrl(original);
+    res.redirect(original);
+    
+  } catch (error) {
+    console.error("Error during redirection:", error);
+    res.status(500).send("Server error");
+  }
+};
--- server/src/services/shortUrl.service.js
+++ server/src/services/shortUrl.service.js
@@ -1,15 +1,19 @@
 import { saveCustomShortUrl, saveShortUrl } from "../dao/shortUrl.dao.js";
 import { generateNanoId } from "../utils/solver.js";
-
+import { redisClient } from "../cache/redisClient.js";
+const CACHE_EXPIRY = 24 * 60 * 60 * 7;
 
 export const shortUrlserviceWithOutUser =async(url)=>{
  const shortUrl = generateNanoId(7); 
+ await redisClient.set(shortUrl, url, { EX: CACHE_EXPIRY });
+ console.log("Redis isOpen:", redisClient.isOpen);
  await  saveShortUrl(shortUrl,url);
 return shortUrl;
 }
 
 export const shortUrlserviceWithUser =async(url,user)=>{
  const shortUrl = generateNanoId(7); 
+ await redisClient.set(shortUrl, url, { EX: CACHE_EXPIRY });
  await  saveShortUrl(shortUrl,url,user);
 return shortUrl;
 }
@@ -17,6 +21,7 @@ return shortUrl;
 export const shortUrlserviceCustom =async(url,user,customId)=>{
  let shortUrl = await generateNanoId(0);
  shortUrl=shortUrl+customId; 
+ await redisClient.set(shortUrl, url, { EX: CACHE_EXPIRY });
  await  saveCustomShortUrl(shortUrl,url,user);
 return shortUrl;
-}
+}   
