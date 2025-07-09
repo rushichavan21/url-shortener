4b17973ae91cf25f714db089ef6abe230c649316
rushichavan21 <rushichavan2327@gmail.com>
2025-07-09 12:06:52 +0530

    cors update

--- server/index.js
+++ server/index.js
@@ -11,7 +11,14 @@ import connectToRedis from './src/cache/redisClient.js';
 import rateLimiter from "./src/middleware/ratelimiter.js";
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "http://localhost:8080","https://shawty-url.vercel.app/"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
+app.options("*", cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
